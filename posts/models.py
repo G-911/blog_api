dfbd019ab4948c30355e2b0d1af54ca210eaf5da
G-911 <g.avila-2002@hotmail.com>
2024-03-02 11:59:59 -0400

    swagger

--- posts/models.py
+++ posts/models.py
@@ -8,6 +8,6 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete = models.CASCADE)
     created_at = models.DateTimeField(auto_now_add = True)
     update_at = models.DateTimeField(auto_now_add = True)
-
+    
     def __str__(self) -> str:
         return self.title
